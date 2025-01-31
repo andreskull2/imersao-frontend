e8e6a8d36719603f2ee1d1b658cf96e7a3f63a77
andreskull2 <andreskull1@gmail.com>
2025-01-31 19:38:07 -0300

    adjust

--- search.js
+++ search.js
@@ -1,52 +1,35 @@
-const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
-const resultPlaylist = document.getElementById('result-playlists');
+const playlistContainer = document.getElementById("result-playlists");
+const searchInput = document.getElementById("search-input");
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
-    fetch(url)
-        .then((response) => response.json())
-        .then((result) => displayResults(result, searchTerm));
+  fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+    .then((response) => response.json())
+    .then((results) => displayResults(results));
 }
 
-function displayResults(result, searchTerm) {
-    resultPlaylist.classList.add("hidden");
-    const gridContainer = document.querySelector('.grid-container');
-    gridContainer.innerHTML = ''; // Limpa os resultados anteriores    
+function displayResults(results) {
+  hidePlaylists();
+  const artistImage = document.getElementById("artist-img");
+  const artistName = document.getElementById("artist-name");
 
-    const filteredArtists = result.filter(artist => artist.name.toLowerCase().includes(searchTerm) );
-
-
-  filteredArtists.forEach(artist => {    
-      const artistCard = document.createElement('div');
-      artistCard.classList.add('artist-card');
-
-      artistCard.innerHTML = `
-          <div class="card-img">
-              <img class="artist-img" src="${artist.urlImg}" />
-              <div class="play">
-                  <span class="fa fa-solid fa-play"></span>
-              </div>
-          </div>
-      <div class="card-text">              
-              <span class="artist-name">${artist.name}</span>
-              <span class="artist-categorie">Artista</span>
-          </div>
-      `;
-       gridContainer.appendChild(artistCard);
+  results.forEach((element) => {
+    artistImage.src = element.urlImg;
+    artistName.innerText = element.name;
   });
-
-  resultArtist.classList.remove('hidden');
+  resultArtist.classList.remove("hidden");
 }
 
-document.addEventListener('input', function () {
-    const searchTerm = searchInput.value.toLowerCase().trim();
+function hidePlaylists() {
+  playlistContainer.classList.add("hidden");
+}
 
-    if (searchTerm === '') {
-        resultPlaylist.classList.remove('hidden');
-        resultArtist.classList.add('hidden');
-        return;
-    }
-
-    requestApi(searchTerm);
+searchInput.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    resultArtist.classList.add("hidden");
+    playlistContainer.classList.remove("hidden");
+    return;
+  }
+  requestApi(searchTerm);
 });
